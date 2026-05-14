1de625190507c1e889c8b7bb8f64a45e27b07743
华 <sliphua@msn.com>
2026-05-15 01:13:02 +0800

    build: use tsdown to build, update all dev deps

---
+++ tsdown.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'tsdown'
+
+export default defineConfig({
+  entry: 'src/index.ts',
+  target: 'es2022',
+  platform: 'neutral',
+  minify: true,
+  exports: true,
+  tsconfig: 'tsconfig.build.json',
+  dts: {
+    build: true,
+  },
+})
